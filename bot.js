84038d172ffb0f07a66e2c58957c0251e0ed8fdc
redpanda25 <rjmantilla@gmail.com>
2020-09-04 15:47:16 -0500

    first deploy test

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const Equation = require('equations').default; 
+
+const client = new Discord.Client();
+client.login("NzUxNTI3ODYxOTQ4NjQ1NTA3.X1KY3Q.RLpMdfJJJzwfMFnvRDlKXKaHz6M");
+
+client.on('ready', async () => {
+    console.log(`Logged in successfully as bot!`);
+    });
+    
+client.on('message', (msg) => {
+    msg.content = msg.content.trim(); // remove extra whitespace
+    const flag = '!solve ';
+    if (msg.content.startsWith(flag)) {
+        const toSolve = msg.content.slice(flag.length, msg.content.length);
+        try {
+            const solved = Equation.solve(toSolve);
+
+            msg.reply(`${toSolve} = ${solved}`); // backticks (TLs) are used to embed variables in strings like `${var}`
+        } catch (err) {
+            msg.reply(`Could not solve the equation '${toSolve}'.`);
+        }
+    }
+});
